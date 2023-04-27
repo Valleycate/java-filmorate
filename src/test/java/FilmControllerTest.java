21a87c8f8af3c454a7c61d670f6476323e229a69
Маша <mariiap.pl@yandex.ru>
2023-04-27 13:10:57 +0300

    fix: исправление checkstyle в test

--- src/test/java/FilmControllerTest.java
+++ src/test/java/FilmControllerTest.java
@@ -1,10 +1,8 @@
+import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.model.Film;
-import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,157 +10,158 @@ public class FilmControllerTest {
     FilmController filmController = new FilmController();
 
     @Test
-    public void shouldCreateFilm(){
+    public void shouldCreateFilm() {
         try {
             Film film = null;
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
         }
         assertEquals(0, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException ignored) {
         }
         assertEquals(0, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setName(" ");
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Неверное имя", e.getMessage());
         }
         assertEquals(0, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setName("Титаник");
-            film.setDescription("0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"
-                    +"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"
-                    +"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789" + "0");
+            film.setDescription("0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789"
+                    + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789"
+                    + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0");
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Описание слишком большое", e.getMessage());
         }
         assertEquals(0, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setName("Титаник");
             film.setDescription("Мелодрамма");
             film.setDuration(-10);
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Продолжительность фильма должна быть положительной", e.getMessage());
         }
         assertEquals(0, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setName("Титаник");
             film.setDescription("Мелодрамма");
             film.setDuration(60);
-            film.setReleaseDate(LocalDate.of(1894,2,28));
+            film.setReleaseDate(LocalDate.of(1894, 2, 28));
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Дата релиза не может быть раньше 28 декабря 1895 года", e.getMessage());
         }
         assertEquals(0, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setName("Титаник");
             film.setDescription("Мелодрамма");
             film.setDuration(60);
-            film.setReleaseDate(LocalDate.of(1999,2,28));
+            film.setReleaseDate(LocalDate.of(1999, 2, 28));
             filmController.create(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
         }
         assertEquals(1, filmController.findAll().size());
     }
+
     @Test
-    public void shouldUpdateFilm(){
+    public void shouldUpdateFilm() {
         Film newFilm = new Film();
         newFilm.setName("Титаник");
         newFilm.setDescription("Мелодрамма");
         newFilm.setDuration(60);
-        newFilm.setReleaseDate(LocalDate.of(1999,2,28));
+        newFilm.setReleaseDate(LocalDate.of(1999, 2, 28));
         filmController.create(newFilm);
         try {
             Film film = null;
             filmController.update(film);
-        }catch (RuntimeException e){
-            assertEquals("Такого фильма нет!",e.getMessage());
+        } catch (RuntimeException e) {
+            assertEquals("Такого фильма нет!", e.getMessage());
         }
         assertEquals(1, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             filmController.update(film);
-        }catch (RuntimeException e){
-            assertEquals("Такого фильма нет!",e.getMessage());
+        } catch (RuntimeException e) {
+            assertEquals("Такого фильма нет!", e.getMessage());
         }
         assertEquals(1, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setId(newFilm.getId());
             film.setName(" ");
             filmController.update(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Неверное имя", e.getMessage());
         }
         assertEquals(1, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setId(newFilm.getId());
             film.setName("Титаник");
-            film.setDescription("0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"
-                    +"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789"
-                    +"0123456789"+"0123456789"+"0123456789"+"0123456789"+"0123456789" + "0");
+            film.setDescription("0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789"
+                    + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789"
+                    + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0123456789" + "0");
             filmController.update(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Описание слишком большое", e.getMessage());
         }
         assertEquals(1, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setId(newFilm.getId());
             film.setName("Титаник");
             film.setDescription("Мелодрамма");
             film.setDuration(-10);
             filmController.update(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Продолжительность фильма должна быть положительной", e.getMessage());
         }
         assertEquals(1, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setId(newFilm.getId());
             film.setName("Титаник");
             film.setDescription("Мелодрамма");
             film.setDuration(60);
-            film.setReleaseDate(LocalDate.of(1894,2,28));
+            film.setReleaseDate(LocalDate.of(1894, 2, 28));
             filmController.update(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Дата релиза не может быть раньше 28 декабря 1895 года", e.getMessage());
         }
         assertEquals(1, filmController.findAll().size());
 
-        try{
+        try {
             Film film = new Film();
             film.setName("Титаник");
             film.setDescription("Мелодрамма");
             film.setDuration(60);
-            film.setReleaseDate(LocalDate.of(1999,2,28));
+            film.setReleaseDate(LocalDate.of(1999, 2, 28));
             filmController.create(film);
             filmController.update(film);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
         }
         assertEquals(2, filmController.findAll().size());
     }
--- src/test/java/UserControllerTest.java
+++ src/test/java/UserControllerTest.java
@@ -1,9 +1,8 @@
+import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.model.User;
-import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,60 +10,60 @@ public class UserControllerTest {
     UserController userController = new UserController();
 
     @Test
-    public void shouldCreateUser(){
+    public void shouldCreateUser() {
         try {
             User user = null;
             userController.create(user);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
         }
         assertEquals(0, userController.findAll().size());
 
-        try{
+        try {
             User user = new User();
             userController.create(user);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
         }
         assertEquals(0, userController.findAll().size());
 
-        try{
+        try {
             User user = new User();
             user.setLogin(" ");
             userController.create(user);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Неверный логин", e.getMessage());
         }
         assertEquals(0, userController.findAll().size());
 
-        try{
+        try {
             User user = new User();
             user.setLogin("Anim");
             user.setEmail("Email.com");
             userController.create(user);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Неверный адрес электронной почты", e.getMessage());
         }
         assertEquals(0, userController.findAll().size());
 
-        try{
+        try {
             User user = new User();
             user.setLogin("Anim");
             user.setEmail("Email.com@Anim");
             user.setName("Маша");
-            user.setBirthday(LocalDate.of(2222,2,28));
+            user.setBirthday(LocalDate.of(2222, 2, 28));
             userController.create(user);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             assertEquals("Дата рождения не может быть в будущем", e.getMessage());
         }
         assertEquals(0, userController.findAll().size());
 
-        try{
+        try {
             User user = new User();
             user.setLogin("Anim");
             user.setEmail("Email.com@Anim");
             user.setName("Маша");
-            user.setBirthday(LocalDate.of(2000,2,28));
+            user.setBirthday(LocalDate.of(2000, 2, 28));
             userController.create(user);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
         }
         assertEquals(1, userController.findAll().size());
 
@@ -72,12 +71,13 @@ public class UserControllerTest {
         user.setLogin("Anim");
         user.setEmail("Email.com@Anim");
         user.setName("");
-        user.setBirthday(LocalDate.of(2000,2,28));
+        user.setBirthday(LocalDate.of(2000, 2, 28));
         userController.create(user);
         assertEquals("Anim", user.getName());
 
         assertEquals(2, userController.findAll().size());
     }
+
     @Test
     public void shouldUpdateUser() {
         User newUser = new User();
@@ -90,7 +90,7 @@ public class UserControllerTest {
             User user = null;
             userController.update(user);
         } catch (RuntimeException e) {
-            assertEquals("Такого пользователя нет!",e.getMessage());
+            assertEquals("Такого пользователя нет!", e.getMessage());
         }
         assertEquals(1, userController.findAll().size());
 
@@ -98,7 +98,7 @@ public class UserControllerTest {
             User user = new User();
             userController.update(user);
         } catch (RuntimeException e) {
-            assertEquals("Такого пользователя нет!",e.getMessage());
+            assertEquals("Такого пользователя нет!", e.getMessage());
         }
         assertEquals(1, userController.findAll().size());
 
@@ -152,7 +152,7 @@ public class UserControllerTest {
         user.setLogin("Anim");
         user.setEmail("Email.com@Anim");
         user.setName("");
-        user.setBirthday(LocalDate.of(2000,2,28));
+        user.setBirthday(LocalDate.of(2000, 2, 28));
         userController.create(user);
         userController.update(user);
         assertEquals("Anim", user.getName());
