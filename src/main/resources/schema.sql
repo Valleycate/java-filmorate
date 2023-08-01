878d201a502178d63d2847c5d6001a2b82d404f2
Маша <mariiap.pl@yandex.ru>
2023-08-01 10:35:31 +0300

    feat: обновление бд - добавление таблицы режисёров и ленты новостей

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -40,3 +40,22 @@ CREATE TABLE IF NOT EXISTS Film_genre(
      film_id INTEGER REFERENCES Film (id),
      genre_id INTEGER REFERENCES Genre (id)
 );
+CREATE TABLE IF NOT EXISTS director(
+    director_id INTEGER PRIMARY KEY,
+    name varchar(1000)
+)
+CREATE TABLE IF NOT EXISTS film_director(
+    film_director_id INTEGER PRIMARY KEY,
+    director_id INTEGER REFERENCES director(director_id),
+    film_id INTEGER REFERENCES Film (id)
+)
+
+CREATE TABLE IF NOT EXISTS feed(
+    feed_id INTEGER PRIMARY KEY,
+    event_id INTEGER,
+    user_id INTEGER REFERENCES Users (id),
+    entity_id INTEGER,
+    event_type ENUM("FRIEND", "REVIEW", "LIKE"),
+    operation ENUM("ADD", "REMOVE", "UPDATE"),
+    feed_time TIMESTAMP
+)
